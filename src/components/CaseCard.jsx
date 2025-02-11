56fd24e959528ed7219237941df20a87fbbf23cd
samerspc <mas.norvg13@gmail.com>
2025-02-11 20:39:44 +0300

    add slider

--- src/components/CaseCard.jsx
+++ src/components/CaseCard.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import ImageSlider from './ImageSlider';
+
 import '../styles/CaseCard.css';
 import '../styles/CaseCardFocus.css'
 
@@ -7,8 +9,14 @@ function CaseCard({caseItem, theme}) {
 
     const [open, setOpen] = useState(false);
 
-    const handleModalOpen = () => {setOpen(true);}
-    const handleModalClose = () => {setOpen(false);}
+    const handleModalOpen = () => {
+        setOpen(true);
+        document.body.classList.add("overflowHidden");
+    }
+    const handleModalClose = () => {
+        setOpen(false);
+        document.body.classList.remove("overflowHidden");
+    }
 
     return (
         <>
@@ -22,8 +30,21 @@ function CaseCard({caseItem, theme}) {
         <div className={`CaseCard-modal-layer ${open ? 'open' : ''}`} onClick={() => handleModalClose()}>
             
         </div>
-        <div className={`CaseCard-modal-wrapper ${open ? 'open' : ''} `}>
+        <div className={`CaseCard-modal-wrapper ${open ? 'open' : ''} ${theme ? 'modalWrapperL' : 'modalWrapperD'}`}>
 
+            <div className={`CaseCard-modal-wrapper-close ${theme ? 'CaseCard-modal-wrapper-closeL' : 'CaseCard-modal-wrapper-closeD'}`} onClick={() => handleModalClose()}>
+                Закрыть
+            </div>
+            
+            <div className='CaseCard-modal-wrapper-info'>
+                <h1>{caseItem.name}</h1>
+                <h2>{caseItem.long}</h2>
+                <a href="#" className={`${ theme ? 'CaseCard-modal-wrapper-info-aL' : 'CaseCard-modal-wrapper-info-aD'}`}>
+                    {caseItem.link}
+                </a>
+            </div>
+
+            <ImageSlider images={[caseItem.img, caseItem.img1]}/>
         </div>
         </>
     )
