341b400cd34c0f00950ccf42b121a7d04aac04ae
samerspc <mas.norvg13@gmail.com>
2025-02-12 20:34:42 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/kaya.pro/',
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',        
+    port: 4173,              
+    allowedHosts: ['girproject.ru'],
+  },
 })
